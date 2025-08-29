e8c8b41d4cf49703ea26509b6c4d407b17291d02
Gillian-LHorset <pq60soi@eduvaud.ch>
2025-08-29 11:38:15 +0200

    feat: modification du programme en console pour ajouter une technologie orienté objet

--- personnel/snail/Exercice/SnailExercice/SnailExercice/Program.cs
+++ personnel/snail/Exercice/SnailExercice/SnailExercice/Program.cs
@@ -1,32 +1,63 @@
 ﻿namespace SnailExercice
 {
+    
+
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            const int SLEEPTIME = 600;
+
+            Snail escargot = new Snail();
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.CursorVisible = false;
             Console.SetCursorPosition(0, 10);
-            Console.Write("__@__ö");
+            Console.Write(escargot._body);
 
             Random rnd = new Random();
-            int randomNumberX = rnd.Next(10, 40);
+            int randomNumberX = rnd.Next(escargot._lifetimeRangeFirst, escargot._lifetimeRangeLast);
             int randomNumberY = rnd.Next(3, 30);
 
             for (int i = 0; i < randomNumberX; i++)
             {
                 Console.Clear();
                 Console.SetCursorPosition(i, randomNumberY);
-                Console.Write("__@__ö");
-                Thread.Sleep(750);
+                Console.Write(escargot._body);
+                Thread.Sleep(SLEEPTIME);
             }
 
-            Thread.Sleep(750);
+            Thread.Sleep(SLEEPTIME);
             Console.Clear();
 
             Console.SetCursorPosition(randomNumberX, randomNumberY);
             Console.Write("______");
             Console.Read();
+            
         }
     }
+
+    class Snail
+    {
+        public string _body;
+        public int _lifetimeRangeFirst;
+        public int _lifetimeRangeLast;
+
+        public Snail()
+        {
+            this._body = "__@__ö";
+            this._lifetimeRangeFirst = 5;
+            this._lifetimeRangeLast = 40;
+        }
+
+        public void start(string body, int lifetime)
+        {
+
+        }
+
+    }
+
 }
