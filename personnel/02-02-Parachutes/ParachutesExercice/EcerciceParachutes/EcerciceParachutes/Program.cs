ea7cf16f8ce971d6cdb43ed8cb77650686dd972a
Gillian-LHorset <pq60soi@eduvaud.ch>
2025-09-05 09:13:54 +0200

    feat(exercice parachute): Création du projet et ajout du redimensionnement de la fenêtre au lancement du programme
    La fenêtre de la console prends une taille définie au lancement du programme.

---
+++ personnel/02-02-Parachutes/ParachutesExercice/EcerciceParachutes/EcerciceParachutes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace EcerciceParachutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+
+
+        }
+    }
+
+    static class Config
+    {
+        public const int SCREEN_HEIGHT = 40;
+        public const int SCREEN_WIDTH = 150;
+    }
+
+
+    
+
+}
